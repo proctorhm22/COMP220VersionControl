e22b706f4ebf9c637790a768f9ea0ac2a391eb2a
BREDENBERGSG22 <BredenbergSG22@gcc.edu>
2023-02-28 12:04:55 -0500

    Updated to request user's name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hello" + name + "!");
     }
 }
